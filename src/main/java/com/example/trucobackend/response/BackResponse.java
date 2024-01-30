7c887825cf410d0ba88c00a1e04c4537f3cf55a8
mateusquixada <mateusquixada@gmail.com>
2024-01-30 12:05:45 -0300

    feat: Adiciona a classe BackResponse

--- src/main/java/com/example/trucobackend/response/BackResponse.java
+++ src/main/java/com/example/trucobackend/response/BackResponse.java
@@ -1,2 +1,15 @@
-package com.example.trucobackend.response;public class BackResponse {
+package com.example.trucobackend.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BackResponse {
+    String status;
+    String message;
+    public BackResponse(String status, String message) {
+        this.status = status;
+        this.message = message;
+    }
 }
