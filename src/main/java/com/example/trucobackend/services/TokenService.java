d7881286010a0000d1e8a1872b58b591b3703d5c
mateusquixada <mateusquixada@gmail.com>
2024-02-07 13:39:11 -0300

    feat: Adiciona geracao de token

--- src/main/java/com/example/trucobackend/services/TokenService.java
+++ src/main/java/com/example/trucobackend/services/TokenService.java
@@ -1,2 +1,33 @@
-package com.example.trucobackend.services;public class TokenService {
+package com.example.trucobackend.services;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.example.trucobackend.user.UserLogin;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    public String generateToken(UserLogin userLogin){
+        try {
+            var algorithm = Algorithm.HMAC256("12345678");
+            return JWT.create()
+                    .withIssuer("API Truco")
+                    .withSubject(userLogin.getLogin())
+                    .withClaim("id", userLogin.getId())
+                    .withExpiresAt(expirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Erro ao gerar Token!", exception);
+        }
+    }
+
+    private Instant expirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
 }
